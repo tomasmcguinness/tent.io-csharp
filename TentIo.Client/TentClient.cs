c9f8a083134a43ed6a11dba9baf413b585db3e7a
Tomas McGuinness <tomas@tomasmcguinness.com>
2012-09-11 07:08:23 +0100

    Added a test project

---
+++ TentIo.Client/TentClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TentIo.Client
+{
+    public class TentClient
+    {
+        private TentClient()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns an instance of a TentClient connected to the target server.
+        /// </summary>
+        /// <param name="serverName"></param>
+        public static TentClient Discover(string serverName)
+        {
+            HttpClient 
+            return new TentClient();
+        }
+
+        public void Register()
+        {
+
+
+        }
+
+        public void Follow(string serverName)
+        {
+
+        }
+
+        public void Unfollow(string serverName)
+        {
+
+        }
+
+        public void PostUpdate(string statusUpdate)
+        {
+
+        }
+
+    }
+}
